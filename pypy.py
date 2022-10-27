029367e63a1ee0127db4ca344c96bf3f7ca85b73
Jean Sidati <jean-sidati.thepaut@etu.minesparis.psl.eu>
2022-10-27 11:46:27 +0200

    cadeau

--- pypy.py
+++ pypy.py
@@ -1,1 +1,40 @@
-hello world
+"hello world"
+import random
+import pygame
+import sys
+
+
+pygame.init()
+screen = pygame.display.set_mode([600, 600])
+clock = pygame.time.Clock()
+def is_white(i,j):
+    return (i+j)%2==0
+width = 20
+height = 20
+red = 255
+green = 255
+blue = 255
+Blanc = [255, 255, 255]
+Noir = [0,0,0]
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_q:
+                pygame.quit()
+                sys.exit()
+    for i in range(30):
+        x=20*i
+        for j in range (30):
+            y=20*j
+            if (i+j)%2==0:
+                rect = [x, y, width, height]
+                red = random.randint(0, 255)
+                green = random.randint(0, 255)
+                blue = random.randint(0, 255)
+                color = [red, green, blue]
+                pygame.draw.rect(screen, color, rect)
+        pygame.display.update()
+        clock.tick(0)
